15002e4ce77ae145d595d9483e3aa188fb67e5a8
JT Reyes <jesustgreyes@gmail.com>
2023-07-14 21:30:50 -0700

    add api'

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,88 @@ $(document).ready(function() {
     var lat;
     var long;
 
+    function getLat(name) {
+        return new Promise((resolve, reject) => {
+          var url = "http://api.openweathermap.org/geo/1.0/direct?q=" + name + "&limit=5&appid=9bce6875713db412816a04531af13ead";
+          fetch(url)
+            .then(function(response) {
+              return response.json();
+            })
+            .then(function(data) {
+              var lat = data[0].lat;
+              resolve(lat);
+            })
+            .catch(function(error) {
+              reject(error);
+            });
+        });
+    }
+
+    function getLon(name) {
+        return new Promise((resolve, reject) => {
+          var url = "http://api.openweathermap.org/geo/1.0/direct?q=" + name + "&limit=5&appid=9bce6875713db412816a04531af13ead";
+          fetch(url)
+            .then(function(response) {
+              return response.json();
+            })
+            .then(function(data) {
+              var lon = data[0].lon;
+              resolve(lon);
+            })
+            .catch(function(error) {
+              reject(error);
+            });
+        });
+    }
+
+    function renderSearch(name, latitude, longitude){
+        if (name === ""){
+            return;
+        }
+        else {
+            var newEl = $("<button>");
+            newEl.text(name);
+            newEl.attr("class", "historyBtn");
+            $("#history").append(newEl);
+            newEl.on("click", function(){
+              $("#genCards").html("");
+              renderMain(name, latitude, longitude);
+            });
     
+        }
+        localStorage.setItem(name, JSON.stringify({lat: latitude, lon: longitude}));
+    }
+
+    function renderMain(name, latitude, longitude){
+        $("#main h2").text(name);
+    
+        var url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=87e016e5e89b444a3ab62dbf9f034527&units=imperial";
+        
+        fetch(url)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function (data){
+                var dateEl = dayjs().format('MM/DD/YY');
+                $("#main h2").text(name + " " + dateEl);
+                var imgUrl = "https://openweathermap.org/img/wn/" + data.weather[0].icon +".png";
+                var image = $('<img>').attr('src', imgUrl).attr('alt', 'Weather Icon');
+                $('#main h4').first().empty();
+                $('#main h4').first().append(image);
+  
+                $("#main h4").eq(1).text("Temp: "+ data.main.temp + " °f");
+                $("#main h4").eq(2).text("Wind: "+ data.wind.speed + " mph");
+                $("#main h4").eq(3).text("Humidity: "+ data.main.humidity + " %");
+                renderFiveDay(name, latitude, longitude);
+            });
+    }
+
+    searchBtnEl.on("click", async function() {
+        var inputCity = $("#inputCity").val();
+        var lat = await getLat(inputCity);
+        var lon = await getLon(inputCity);
+        renderSearch(inputCity, lat, lon);
+        renderMain(inputCity, lat, lon);
+      });
 
 });
